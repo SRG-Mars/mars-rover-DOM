324702322cea9cc562ef3a74e3f9ac0bc19f8ce9
robbiesanchez <rfsanchez004@gmail.com>
2019-10-28 15:01:00 -0400

    rover commands

--- script.js
+++ script.js
@@ -1,7 +1,34 @@
 class Rover{
-    constructor(position, direction){
-      this.position = position;
+    constructor(direction, x, y){
+      this.x = x;
+      this.y = y;
       this.direction = direction;
     }
-  
+
+// Commands function
+    
+    roverCommands(orders){
+        for (let i = 0; i < orders.length; i++){
+          let order = orders[i];
+          switch(order) {
+            case "r":
+              this.turnRight;
+              break;
+            case "l":
+              this.turnLeft;
+              break;
+            case "f":
+              this.moveForward;
+              break;
+            case "b":
+              this.moveBack;
+              break;
+            default:
+              console.log("Not a valid command!");
+          }
+        }
+      }
+
   }
+
+  
