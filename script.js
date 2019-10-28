a372cdf14a7366693cea367dc58e3ccf2a1a5a7b
gdel21 <56038834+gdel21@users.noreply.github.com>
2019-10-28 15:13:33 -0400

    fix

--- script.js
+++ script.js
@@ -1,9 +1,34 @@
 class Rover{
+
     constructor(direction, x, y){
       this.x = x;
       this.y = y;
       this.correctDir(direction);
     }
+
+    correctDir(direction){ 
+        let numericDir = 1;
+        if(direction === "W"){
+            numericDir = 4;
+        }
+        if(direction === "S"){
+            numericDir = 3;
+        }
+        if(direction === "E"){
+            numericDir = 2;
+        }
+        if(direction === "N"){
+            numericDir = 1;
+        }
+        if(direction > 4){
+            this.direction = 1;
+        }
+        
+        if (direction < 1){
+            this.direction = 4;
+        }
+      }
+   
 
     //Turns rover left
     turnLeft(){
