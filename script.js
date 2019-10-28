5f7781f822b3994a59a9f4e6a61af5d2f23b7c8c
gdel21 <56038834+gdel21@users.noreply.github.com>
2019-10-28 15:01:37 -0400

    turning done

--- script.js
+++ script.js
@@ -1,7 +1,50 @@
 class Rover{
-    constructor(position, direction){
-      this.position = position;
-      this.direction = direction;
+    constructor(direction, x, y){
+      this.x = x;
+      this.y = y;
+      this.correctDir(direction);
     }
+
+//Direction function
+    correctDir(direction){ 
+        let numericDir = 1;
+        if(direction === "W"){
+            numericDir = 4;
+        }
+        if(direction === "S"){
+            numericDir = 3;
+        }
+        if(direction === "E"){
+            numericDir = 2;
+        }
+        if(direction === "N"){
+            numericDir = 1;
+        }
+        if(direction > 4){
+          this.direction = 1;
+        }
+      
+        if (direction < 1){
+          this.direction = 4;
+        }
+      }
+
+      //Turns rover left
+      turnLeft(){
+        this.direction--;
+      }
+      
+      //Turns rover right
+      turnRight(){
+        this.direction++;
+      }
+
+
+
+
   
   }
+
+  let first = new Rover("W", 1,1);
+
+  first.roverCommands("fbblrfbf");
