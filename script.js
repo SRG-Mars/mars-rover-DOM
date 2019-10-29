f06367d96e0dff2b821843b26316d0c3c9131afe
gdel21 <56038834+gdel21@users.noreply.github.com>
2019-10-29 15:21:39 -0400

    board and movement

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ class Rover {
       this.name = name;
       this.direction = 1;
       this.lastPosition = [];
-      currentBoard.placeRover(this);
     }
   
     correctDir(direction){ 
@@ -89,9 +88,9 @@ class Rover {
     isAbleToMove(command) {
         if (command === "f") {
         if (this.direction === 1 && this.y === 0) return false;
+        else if (this.direction === 2 && this.x === 9) return false;
         else if (this.direction === 3 && this.y === 9) return false;
         else if (this.direction === 4 && this.x === 0) return false;
-        else if (this.direction === 2 && this.x === 9) return false;
         else return true;
         } else if (command === "b") {
         if (this.direction === 3 && this.y === 0) return false;
@@ -103,80 +102,76 @@ class Rover {
     }
 
 
+
     moveForward (){
         if (!this.isAbleToMove("f")) return console.log("Rover can't go outside of the board!");
-        if (this.direction === 1) this.y--;
-        else if (this.direction === 2)  this.x++;
-        else if (this.direction === 3)  this.y++;
-        else if (this.direction === 4) this.x--;
-        this.moveOnBoard();
+        if (this.direction === 1) {
+            this.y--;
+            currentBoard.theBoard[this.x][this.y-1] = this.name;
+            currentBoard.theBoard[this.x][this.y] = "free";
+
+        }
+        else if (this.direction === 2)  {
+            this.x++;
+            currentBoard.theBoard[this.x+1][this.y] = this.name;
+            currentBoard.theBoard[this.x][this.y] = "free";
+
+        }
+        else if (this.direction === 3)  {
+            this.y++;
+            currentBoard.theBoard[this.x][this.y+1] = this.name;
+            currentBoard.theBoard[this.x][this.y] = "free";
+
+        }
+        else if (this.direction === 4) {
+            this.x--;
+            currentBoard.theBoard[this.x-1][this.y] = this.name;
+            currentBoard.theBoard[this.x][this.y] = "free";
+
+        }
     };
     moveBack() {
         if (!this.isAbleToMove("b")) return console.log("Rover can't go outside of the board!");
-        if (this.direction === 1) this.y++;
-        else if (this.direction === 2) this.x--;
-        else if (this.direction === 3) this.y--;
-        else if (this.direction === 4) this.x++;
-        this.moveOnBoard();
+        if (this.direction === 1) {
+            this.y++;
+            currentBoard.theBoard[this.x][this.y+1] = this.name;
+            currentBoard.theBoard[this.x][this.y] = "free";
+        }
+        else if (this.direction === 2) {
+            this.x--;
+            currentBoard.theBoard[this.x-1][this.y] = this.name;
+            currentBoard.theBoard[this.x][this.y] = "free";
+        }
+        else if (this.direction === 3) {
+            this.y--;
+            currentBoard.theBoard[this.x][this.y-1] = this.name;
+            currentBoard.theBoard[this.x][this.y] = "free";
+        }
+        else if (this.direction === 4) {
+            this.x++;
+            currentBoard.theBoard[this.x+1][this.y] = this.name;
+            currentBoard.theBoard[this.x][this.y] = "free";
+        }
     }
 
     getInfo(){
     console.log(this.getDirection(), this.y, this.x);
     }
-
-    getBoardPosition(){
-        let temp;
-        for(let a = 0; a < currentBoard.currentBoard.length; a++){
-            console.log(currentBoard.currentBoard)
-            for(let b = 0; b < currentBoard.currentBoard[a].length; b++){
-                console.log(currentBoard.currentBoard[1][1])
-                if(currentBoard.currentBoard[a][b].includes(this.name)){
-                    console.log("success");
-                    return {a,b};
-                }
-            }
-        }
-    }
-
-   
-
-    moveOnBoard(){
-        for(let temp = 0; temp < currentBoard.currentBoard.length; temp++){
-            for(let whatever = 0; whatever < currentBoard.currentBoard[temp].length; whatever++){
-                if(currentBoard.currentBoard[i][whatever].indexOf(this.name)){
-                    currentBoard.currentBoard[i][whatever].splice(currentBoard.currentBoard[i][j], 1, []);
-                    currentBoard.currentBoard[this.x][this.y] = [this.name];
-                }
-            }
-        }
-    }
-
-
   
   }
 
 
-// class Obstacle {
-//     constructor(x,y){
-//         this.x = x;
-//         this.y = y;
-//     }
+class Obstacle {
+    constructor(x,y){
+        this.x = x;
+        this.y = y;
+    }
 
-//     createObst(){
-//         for(i = 0; i < roverTracker.length; i++){
-//             if(this.x === roverTracker[i].x && this.y === roverTracker[i].y){
-//                 this.x = Math.floor(Math.random() * 10);
-//                 this.y = Math.floor(Math.random() * 10);
-//             }
-//         }
-
-//     }
-
-// }
+}
 
 class Board {
     constructor(){
-        this.currentBoard = [];
+        this.theBoard= [];
 
     }
 
@@ -188,12 +183,28 @@ class Board {
                 "free"
                 )
              }
-             this.currentBoard.push(currentCords)
+             this.theBoard.push(currentCords)
            }
     }
 
     placeRover (rover){
-        this.currentBoard[rover.x][rover.y].push(rover.name);
+        console.log(this.theBoard[rover.x][rover.y]);
+        this.theBoard[rover.x][rover.y] = rover.name;
+    }
+
+    createObstacle(){
+        console.log('Obstacle Created');
+        let randomCol = Math.floor(Math.random() * 10);
+        let randomRow = Math.floor(Math.random() * 10);
+
+        console.log(randomCol);
+        console.log(randomRow);
+        console.log(currentBoard.theBoard[randomRow][randomCol]);
+        if(currentBoard.theBoard[randomRow][randomCol] === "free"){
+            currentBoard.theBoard[randomRow][randomCol] = "obstacle";
+        }else{
+            this.createObstacle();
+        }
     }
 
 }
@@ -206,10 +217,12 @@ currentBoard.createBoard();
 
 let first = new Rover("N", 1, 1, "tom");
 
-first.getInfo();
+currentBoard.placeRover(first);
+
+// first.getInfo();
 
 // first.commands("rffflfffffff");
 
-first.getInfo();
+// first.getInfo();
 
-first.getBoardPosition();
+// first.getBoardPosition();
